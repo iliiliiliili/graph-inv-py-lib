e2627b6f93740d35ce0f95fd308dcf4570f14cd1
Illia Oleksiienko <io@ece.au.dk>
2024-05-14 06:38:39 +0000

    Add node+graph level umap

--- src/ginv/transform_graph.py
+++ src/ginv/transform_graph.py
@@ -15,11 +15,15 @@ def transform_graph_for_umap_node_level(graph: OGraph, dtype=np.float32):
 
 def transform_graph_for_umap_connection_level(graph: OGraph, dtype=np.float32):
 
-    result = np.empty([graph.connection_count, 1 + 2 * len(graph.nodes.features)], dtype=dtype)
+    result = np.empty(
+        [graph.connection_count, 1 + 2 * len(graph.nodes.features)], dtype=dtype
+    )
 
     for i in range(len(graph.nodes.features)):
         result[:, 1 + i] = graph.nodes.features[i][graph.connections.froms]
-        result[:, 1 + len(graph.nodes.features) + i] = graph.nodes.features[i][graph.connections.tos]
+        result[:, 1 + len(graph.nodes.features) + i] = graph.nodes.features[i][
+            graph.connections.tos
+        ]
 
     return result
 
@@ -33,3 +37,47 @@ def transform_connection_level_features_to_node_level_features(graph: OGraph, fe
         result[graph.connections.tos[i]] += features[i]
 
     return result
+
+
+def transform_graph_for_umap_node_knn_simple(
+    graph: OGraph,
+    neighbours_count: int,
+    dtype=np.int32,
+    epsilon = 0.0001
+):
+
+    result = np.zeros([graph.node_count, neighbours_count], dtype=dtype)
+    weights = np.ones([graph.node_count, neighbours_count], dtype=dtype) * epsilon
+    sizes = np.zeros([graph.node_count], dtype=np.uint16)
+
+    for i in range(graph.connection_count):
+
+        if (i % 1000) == 0:
+            print(f"Creating simple knn {i}/{graph.connection_count}", end="\r")
+
+        weight = graph.connections.values[i]
+        for idx, other_idx in [
+            (graph.connections.froms[i], graph.connections.tos[i]),
+            (graph.connections.tos[i], graph.connections.froms[i]),
+        ]:
+            if sizes[idx] < neighbours_count:
+
+                result[idx, sizes[idx]] = other_idx
+                weights[idx, sizes[idx]] = weight
+                sizes[idx] += 1
+            else:
+                to_replace = None
+                for q in range(sizes[idx]):
+                    if weights[idx, q] < weight:
+                        if (to_replace is None) or (
+                            weights[idx, to_replace] > weights[idx, q]
+                        ):
+                            to_replace = q
+                if to_replace is not None:
+                    result[idx, to_replace] = other_idx
+                    weights[idx, to_replace] = weight
+
+    print()
+
+    distances = 1.0 / weights
+    return result, distances, sizes
--- src/run_umap.py
+++ src/run_umap.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import umap
-import umap.plot
+from src import umap
+from src.umap import plot
 from sklearn.datasets import load_digits
 from fire import Fire
 import os
@@ -11,10 +11,11 @@ from ginv.transform_graph import (
     transform_graph_for_umap_node_level,
     transform_graph_for_umap_connection_level,
     transform_connection_level_features_to_node_level_features,
+    transform_graph_for_umap_node_knn_simple,
 )
 
 
-def node_umap(node_count=0, plots_dir="./plots/umap"):
+def node_graph_umap(node_count=0, plots_dir="./plots/umap", all_n_neighbours = [64, 32, 16, 8, 4, 2]):
 
     os.makedirs(plots_dir, exist_ok=True)
 
@@ -33,8 +34,52 @@ def node_umap(node_count=0, plots_dir="./plots/umap"):
     labels[positive_labels] = np.floor(logs[positive_labels])
     labels[~positive_labels] = -np.floor(logs[~positive_labels])
 
-    for n_neighbours in [16, 8, 4, 2]:
-        for min_dist in [0.4, 0.2]:
+    for n_neighbours in all_n_neighbours:
+
+            name = f"umap_istanbul_node_and_graph_level_simple_{dataset.node_count}n_{n_neighbours}nb"
+
+            knn, knn_distances, knn_size = transform_graph_for_umap_node_knn_simple(dataset, n_neighbours)
+
+            print(f"Fitting umap for {name}")
+            reducer = umap.UMAP(
+                n_neighbors=n_neighbours,
+                precomputed_knn=(knn, knn_distances)
+            )
+            mapper = reducer.fit(umap_data)
+
+            print(f"Plotting for {name}")
+            umap.plot.points(mapper, labels=labels)
+
+            plt.savefig(f"{plots_dir}/{name}.png")
+
+            print(f"Saving embeddings for {name}")
+            embeddings = reducer.transform(umap_data)
+
+            np.save(f"{plots_dir}/embeddings_{name}", embeddings)
+
+            print()
+
+def node_umap(node_count=0, plots_dir="./plots/umap", all_n_neighbours = [16, 8, 4, 2], all_min_dist = [0.4, 0.2]):
+
+    os.makedirs(plots_dir, exist_ok=True)
+
+    dataset = IstanbulEinDataset("./data/istanbul")
+
+    if node_count > 0:
+        print("Reducing the dataset")
+        dataset.reduce(node_count)
+        print(dataset)
+
+    print("Creating umap_data")
+    umap_data = transform_graph_for_umap_node_level(dataset)
+    labels = np.copy(dataset.node_profits)
+    logs = np.log10(np.abs(labels))
+    positive_labels = labels > 0
+    labels[positive_labels] = np.floor(logs[positive_labels])
+    labels[~positive_labels] = -np.floor(logs[~positive_labels])
+
+    for n_neighbours in all_n_neighbours:
+        for min_dist in all_min_dist:
 
             name = f"umap_istanbul_{dataset.node_count}n_{n_neighbours}nb_{str(min_dist).replace('.', '')}d"
 
@@ -48,7 +93,7 @@ def node_umap(node_count=0, plots_dir="./plots/umap"):
             print(f"Plotting for {name}")
             umap.plot.points(mapper, labels=labels)
 
-            matplotlib.pyplot.savefig(f"{plots_dir}/{name}.png")
+            plt.savefig(f"{plots_dir}/{name}.png")
 
             print(f"Saving embeddings for {name}")
             embeddings = reducer.transform(umap_data)
@@ -58,7 +103,7 @@ def node_umap(node_count=0, plots_dir="./plots/umap"):
             print()
 
 
-def connection_umap(node_count=0, plots_dir="./plots/umap"):
+def connection_umap(node_count=0, plots_dir="./plots/umap", all_n_neighbours = [16, 8, 4, 2], all_min_dist = [0.4, 0.2]):
 
     os.makedirs(plots_dir, exist_ok=True)
 
@@ -77,8 +122,8 @@ def connection_umap(node_count=0, plots_dir="./plots/umap"):
     labels[positive_labels] = np.floor(logs[positive_labels])
     labels[~positive_labels] = -np.floor(logs[~positive_labels])
 
-    for n_neighbours in [16, 8, 4, 2]:
-        for min_dist in [0.4, 0.2]:
+    for n_neighbours in all_n_neighbours:
+        for min_dist in all_min_dist:
 
             name = f"umap_istanbul_connection_level_{dataset.node_count}n_{n_neighbours}nb_{str(min_dist).replace('.', '')}d"
 
