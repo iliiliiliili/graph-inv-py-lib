965bb331a65901fba4a811a64e2a126f3094e8f4
Illia Oleksiienko <io@ece.au.dk>
2024-05-17 23:36:45 +0000

    Add multilevel knn

--- src/ginv/transform_graph.py
+++ src/ginv/transform_graph.py
@@ -2,6 +2,7 @@ from dataclasses import asdict
 import numpy as np
 from scipy.sparse import csr_matrix
 
+from ginv.max_heap import MaxHeap
 from osigma.ograph import OGraph
 
 
@@ -42,10 +43,7 @@ def transform_connection_level_features_to_node_level_features(graph: OGraph, fe
 
 
 def transform_graph_for_umap_node_knn_simple(
-    graph: OGraph,
-    neighbours_count: int,
-    dtype=np.int32,
-    epsilon = 0.0001
+    graph: OGraph, neighbours_count: int, dtype=np.int32, epsilon=0.0001
 ):
 
     result = np.zeros([graph.node_count, neighbours_count], dtype=dtype)
@@ -95,13 +93,15 @@ def transform_graph_for_umap_node_knn_simple(
 def transform_graph_for_umap_node_knn_simple_full(
     graph: OGraph,
     neighbours_count: int,
-    weight_to_distance_scale = 1.0,
+    weight_to_distance_scale=1.0,
     dtype=np.int32,
-    epsilon = 0.0001
+    epsilon=0.0001,
 ):
 
     result = np.zeros([graph.node_count, neighbours_count], dtype=dtype)
-    distances = np.ones([graph.node_count, neighbours_count], dtype=np.float32) / epsilon
+    distances = (
+        np.ones([graph.node_count, neighbours_count], dtype=np.float32) / epsilon
+    )
     sizes = np.zeros([graph.node_count], dtype=np.uint16)
 
     print(f"Creating simple full knn {graph.connection_count}")
@@ -146,20 +146,33 @@ def transform_graph_for_umap_node_knn_simple_full(
             second_level_neighbours = []
             second_level_distances = []
 
-            for q in range(sizes[i]): # result[i] = [a b c ... | 0 0 0]
-                for p in range(sizes[result[i, q]]): # result[i, q] = a; result[a, p] = second_level_neighbour
-                    if (result[result[i, q], p] not in second_level_neighbours) and (result[result[i, q], p] != i) and (result[result[i, q], p] not in result[i]):
+            for q in range(sizes[i]):  # result[i] = [a b c ... | 0 0 0]
+                for p in range(
+                    sizes[result[i, q]]
+                ):  # result[i, q] = a; result[a, p] = second_level_neighbour
+                    if (
+                        (result[result[i, q], p] not in second_level_neighbours)
+                        and (result[result[i, q], p] != i)
+                        and (result[result[i, q], p] not in result[i])
+                    ):
                         second_level_neighbours.append(result[result[i, q], p])
-                        second_level_distances.append(distances[i, q] + distances[result[i, q], p])
-            
+                        second_level_distances.append(
+                            distances[i, q] + distances[result[i, q], p]
+                        )
+
             if len(second_level_neighbours) <= 0:
                 break
 
             q = 0
 
-            sort_ids = [i for (_, i) in sorted((v, i) for (i, v) in enumerate(second_level_distances))]
+            sort_ids = [
+                i
+                for (_, i) in sorted(
+                    (v, i) for (i, v) in enumerate(second_level_distances)
+                )
+            ]
 
-            while ((sizes[i] < neighbours_count) and (q < len(second_level_distances))):
+            while (sizes[i] < neighbours_count) and (q < len(second_level_distances)):
                 neighbour = second_level_neighbours[sort_ids[q]]
                 dist = second_level_distances[sort_ids[q]]
 
@@ -175,24 +188,124 @@ def transform_graph_for_umap_node_knn_simple_full(
     return result, distances
 
 
-def transform_graph_into_a_csr_matrix(graph: OGraph):
+def transform_graph_into_a_csr_matrix(graph: OGraph, weight_to_distance_scale=1.0, epsilon=0.0001):
 
     row = graph.connections.froms
     col = graph.connections.tos
-    val = graph.connections.values
+    val = weight_to_distance_scale / (graph.connections.values + epsilon)
 
     result = csr_matrix((val, (row, col)), shape=(graph.node_count, graph.node_count))
 
-    result = np.empty([graph.node_count, len(graph.nodes.features)], dtype=dtype)
-
     return result
+
+
+def transform_graph_for_umap_node_knn_multilevel(
+    graph: OGraph,
+    neighbours_count: int,
+    weight_to_distance_scale=1.0,
+    dtype=np.int32,
+    epsilon=0.0001,
+):
+
+    result = np.zeros([graph.node_count, neighbours_count], dtype=dtype)
+    distances = (
+        np.ones([graph.node_count, neighbours_count], dtype=np.float32) / epsilon
+    )
+    sizes = np.zeros([graph.node_count], dtype=np.uint16)
+
+    max_heap = MaxHeap()
+
+    print(f"[1/2] Creating multilevel knn {graph.connection_count}")
+
+    for i in range(graph.connection_count):
+
+        if (i % 1000) == 0:
+            print(f"[1/2] Creating multilevel knn {i}/{graph.connection_count}", end="\r")
+
+        dist = weight_to_distance_scale / (graph.connections.values[i] + epsilon)
+
+        for idx, other_idx in [
+            (graph.connections.froms[i], graph.connections.tos[i]),
+            (graph.connections.tos[i], graph.connections.froms[i]),
+        ]:
+            if sizes[idx] < neighbours_count:
+                max_heap.push(other_idx, dist, result[idx], distances[idx], sizes[idx:idx+1])
+            elif max_heap.top_value(distances[idx]) > dist:
+                max_heap.pop(result[idx], distances[idx], sizes[idx:idx+1])
+                max_heap.push(other_idx, dist, result[idx], distances[idx], sizes[idx:idx+1])
+
+    print()
+    print(f"[2/2] Creating multilevel knn {graph.node_count}")
+
+    for i in range(graph.node_count):
+
+        if (i % 1) == 0:
+            print(f"[2/2] Creating multilevel knn {i}/{graph.node_count}", end="\r")
+        
+        updated = True
+
+        while updated:
+
+            updated = False
+
+            second_level_neighbours = []
+            second_level_distances = []
+
+            for q in range(sizes[i]):  # result[i] = [a b c ... | 0 0 0]
+                for p in range(
+                    sizes[result[i, q]]
+                ):  # result[i, q] = a; result[a, p] = second_level_neighbour
+                    if (
+                        (result[result[i, q], p] not in second_level_neighbours)
+                        and (result[result[i, q], p] != i)
+                        and (result[result[i, q], p] not in result[i])
+                    ):
+                        second_level_neighbours.append(result[result[i, q], p])
+                        second_level_distances.append(
+                            distances[i, q] + distances[result[i, q], p]
+                        )
+
+            if len(second_level_neighbours) <= 0:
+                break
+
+            for q in range(len(second_level_distances)):
+
+                neighbour = second_level_neighbours[q]
+                dist = second_level_distances[q]
+
+                if sizes[i] < neighbours_count:
+                    max_heap.push(neighbour, dist, result[i], distances[i], sizes[i:i+1])
+                    updated = True
+                elif dist < max_heap.top_value(distances[i]):
+                    max_heap.pop(result[i], distances[i], sizes[i:i+1])
+                    max_heap.push(neighbour, dist, result[i], distances[i], sizes[i:i+1])
+                    updated = True
+
+
+    sort_ids = np.argsort(distances, axis=-1)
+    result = np.take_along_axis(result, sort_ids, -1)
+    distances = np.take_along_axis(distances, sort_ids, -1)
+
+    return result, distances
+
+
 
 def reduce_knn(knn: np.ndarray, new_neighbours_count: int):
 
     assert knn.shape[1] > new_neighbours_count
 
     result = knn[:, :new_neighbours_count]
-    
+
     return result
 
-    
+
+def save_knn(knn: np.ndarray, knn_dist: np.ndarray, file_knn: str, file_knn_dist: str):
+    np.save(file_knn, knn)
+    np.save(file_knn_dist, knn_dist)
+
+
+def load_knn(file_knn: str, file_knn_dist: str):
+    knn = np.load(file_knn)
+    knn_dist = np.load(file_knn_dist)
+
+    return knn, knn_dist
--- src/run_umap.py
+++ src/run_umap.py
@@ -12,7 +12,11 @@ from ginv.transform_graph import (
     transform_connection_level_features_to_node_level_features,
     transform_graph_for_umap_node_knn_simple,
     transform_graph_for_umap_node_knn_simple_full,
+    transform_graph_for_umap_node_knn_multilevel,
+    transform_graph_into_a_csr_matrix,
     reduce_knn,
+    save_knn,
+    load_knn,
 )
 
 
@@ -32,11 +36,12 @@ def create_umap_data_and_labels(dataset: IstanbulEinDataset, level="node"):
     return umap_data, labels
 
 
-def node_graph_umap(
+def node_graph_sparse_umap(
     node_count=0,
-    knn_method="simple_full",
     plots_dir="./plots/umap",
     all_n_neighbours=[64, 32, 16, 8, 4, 2],
+    all_min_dist=[0.4, 0.2],
+    weight_to_distance_scale=255.0,
     name_suffix="",
 ):
 
@@ -52,9 +57,65 @@ def node_graph_umap(
     print("Creating umap_data")
     umap_data, labels = create_umap_data_and_labels(dataset)
 
+    print("Creating csr matrix")
+    csr_graph = transform_graph_into_a_csr_matrix(dataset)
+
+    all_n_neighbours.sort(reverse=True)
+
+    name_suffix = ("_" + name_suffix) if len(name_suffix) > 0 else ""
+
+    for n_neighbours in all_n_neighbours:
+        for min_dist in all_min_dist:
+
+            name = f"umap_istanbul_node_and_graph_level_csr_{dataset.node_count}n_{n_neighbours}nb_{min_dist}d_{weight_to_distance_scale}ws{name_suffix}"
+
+            print(f"Fitting umap for {name}")
+            reducer = umap.UMAP(
+                n_neighbors=n_neighbours,
+            )
+            mapper = reducer.fit(csr_graph)
+
+            print(f"Plotting for {name}")
+            umap.plot.points(mapper, labels=labels)
+
+            plt.savefig(f"{plots_dir}/{name}.png")
+
+            # print(f"Saving embeddings for {name}")
+            # embeddings = reducer.transform(umap_data)
+            # np.save(f"{plots_dir}/embeddings_{name}", embeddings)
+
+            print()
+
+
+def node_graph_umap(
+    node_count=0,
+    knn_method="multilevel",
+    plots_dir="./plots/umap",
+    all_n_neighbours=[64, 32, 16, 8, 4, 2],
+    name_suffix="",
+    save_and_load_knn=True,
+    knns_dir="./data/knn",
+):
+
+    os.makedirs(plots_dir, exist_ok=True)
+
+    if save_and_load_knn:
+        os.makedirs(knns_dir, exist_ok=True)
+
+    dataset = IstanbulEinDataset("./data/istanbul")
+
+    if node_count > 0:
+        print("Reducing the dataset")
+        dataset.reduce(node_count)
+        print(dataset)
+
+    print("Creating umap_data")
+    umap_data, labels = create_umap_data_and_labels(dataset)
+
     knn_function = {
         "simple": transform_graph_for_umap_node_knn_simple,
         "simple_full": transform_graph_for_umap_node_knn_simple_full,
+        "multilevel": transform_graph_for_umap_node_knn_multilevel,
     }[knn_method]
 
     knn = None
@@ -67,12 +128,27 @@ def node_graph_umap(
     for n_neighbours in all_n_neighbours:
 
         name = f"umap_istanbul_node_and_graph_level_{knn_method}_{dataset.node_count}n_{n_neighbours}nb{name_suffix}"
+        knn_path = f"{knns_dir}/knn_{knn_method}_{dataset.node_count}n_{n_neighbours}nb.npy"
+        knn_dist_path = (
+            f"{knns_dir}/knn_dist_{knn_method}_{dataset.node_count}n_{n_neighbours}nb.npy"
+        )
 
         if knn is None:
-            knn, knn_distances = knn_function(dataset, n_neighbours)
+
+            if save_and_load_knn and os.path.exists(knn_path):
+                knn, knn_distances = load_knn(knn_path, knn_dist_path)
+                print("Loaded knn from file")
+            else:
+                knn, knn_distances = knn_function(dataset, n_neighbours)
+
+                if save_and_load_knn:
+                    save_knn(knn, knn_distances, knn_path, knn_dist_path)
         else:
             knn = reduce_knn(knn, n_neighbours)
             knn_distances = reduce_knn(knn_distances, n_neighbours)
+
+            if save_and_load_knn:
+                save_knn(knn, knn_distances, knn_path, knn_dist_path)
 
         print(f"Fitting umap for {name}")
         reducer = umap.UMAP(
